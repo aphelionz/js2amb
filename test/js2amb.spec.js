9bfcbe710718d121fa6a29521d8f9a4558d8ee15
V-V <vvp@haja.io>
2019-10-07 12:19:23 +0300

    Cleaned up the tests abit

--- test/js2amb.spec.js
+++ test/js2amb.spec.js
@@ -3,21 +3,30 @@ const assert = require('assert')
 const js2amb = require('../src')
 const fs = require('fs')
 const FIXTURES_PATH = 'test/fixtures/js/'
+const normalizeAmbientsCode = code => code.replace(/\r?\n\s*|\r\s*/g, '').replace(/\s+/g, ' ')
 
 describe('JS Compiler', function () {
   it('compiles to ambient syntax', () => {
     const fixtures = fs.readdirSync(FIXTURES_PATH)
-    const len = fixtures.length / 2
-    while (fixtures.length > 0) {
-      console.log(`Compiling: ${fixtures[0].split('.')[0]}`)
-      const javascript = fs.readFileSync(FIXTURES_PATH + fixtures[1]).toString().trim()
-      const result = fs.readFileSync(FIXTURES_PATH + fixtures[0])
+    const fixturePairs = fixtures
+        .filter(item => item.endsWith('.ambient'))
+        .map((filename, index) => {
+          return {
+            ambient: filename,
+            js: fixtures[index * 2 + 1]
+          }
+        })
+
+    fixturePairs.forEach((filePair) => {
+      console.log(`Compiling: ${FIXTURES_PATH + filePair.js}`)
+      const javascript = fs.readFileSync(FIXTURES_PATH + filePair.js).toString().trim()
+      const expectedAmbient = normalizeAmbientsCode(fs.readFileSync(FIXTURES_PATH + filePair.ambient).toString())
       assert.strictEqual(
-        js2amb(javascript),
-        result.toString().replace(/\r?\n\s*|\r\s*/g, '').replace(/\s+/g, ' '),
-        'Test case ' + (len - fixtures.length / 2 + 1) + ' failed'
+          js2amb(javascript),
+          expectedAmbient,
+          `Compiling ${filePair.js} didn't result in ${filePair.ambient}`
       )
-      fixtures.splice(0, 2)
-    }
+
+    })
   })
 })
